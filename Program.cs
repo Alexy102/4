b2f3820314a1785e9bab83a0fe8c48f1618b93f1
Alexy102 <59171672+Alexy102@users.noreply.github.com>
2019-12-23 21:00:03 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static int sum(int x)
+        {
+            if (x > 9 && x < 100) //если число содержит 2 разряда
+                return x / 10 + x % 10;
+            if (x > 99 && (x < 1000)) //если число содержит 3 разряда
+                return x / 100 + x / 10 % 10 + x % 10;
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            bool t;
+            for (int i = 10; i < 100; i++)
+            {
+                t = true;
+                for (int j = 2; j < 10; j++)
+                {
+                    if (sum(i) != sum(i * j))
+                    {
+                        t = false;
+                        break;
+                    }
+                }
+                if (t)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
